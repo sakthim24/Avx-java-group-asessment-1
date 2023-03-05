6235da39e921abb3047b989dcb14528e7951bc66
sakthim24 <sakthivel.m2418@gmail.com>
2023-03-05 17:50:15 +0530

    second function completed

--- Userdetails.java
+++ Userdetails.java
@@ -1,25 +1,27 @@
+import java.util.*;
 
-public class Userdetails extends products
+class Userdetails 
 {
     int user_id;
     String user_name;
     int age;
     String gender ;
- 
+    
+    List<Products> products =new  ArrayList<Products>();
     public Userdetails(int id,String user_name, int age,String gender)
     {
         this.user_id=id;
         this.user_name = user_name;
         this.age = age;
+        this.gender =gender;
     }
  
     void purchase(int id,String product,int month){
-        this.user_id=id;
-        this.monthOfPurchase=month;
-        this.product=product;
+        products.add(new Products(user_id,month,product));
+
     }
     void display(){
-       System.out.println();
+       System.out.println(user_name + " : " + products);
     }
  
     public String getuser_name() {
@@ -29,7 +31,23 @@ public class Userdetails extends products
     public int getAge() {
         return age;
     }
- 
+
+    
+    @Override
+    public String toString()
+    {
+        return "{" +
+                    "user_name='" + user_name + '\'' +
+                    ", age=" + age +
+                    ", purchase=" + products +
+                    '}';
+    }
    
+    static int compareByAge(Userdetails c0, Userdetails c1)
+    {
+        return Integer.compare(c0.age, c1.age);
+    }
 
 }
+
+
